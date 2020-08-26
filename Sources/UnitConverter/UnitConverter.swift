bee307ce2d848248992272f90224ff2f7eb220f2
Spencer Curtis <spencercolecurtis@gmail.com>
2020-08-26 11:32:17 -0600

    Add import Foundation

--- Sources/UnitConverter/UnitConverter.swift
+++ Sources/UnitConverter/UnitConverter.swift
@@ -1,4 +1,5 @@
 // Just because the struct is public, doesn't mean everything inside it is too. You must mark everything you want to be externally accessible with an access control level of public.
+import Foundation
 
 public class UnitConverter {
     
