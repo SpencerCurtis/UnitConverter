ebf2fcc1de3bba4fce4b48d637ec91641bbcc1df
Spencer Curtis <spencercolecurtis@gmail.com>
2020-08-26 11:30:11 -0600

    Change struct UnitConverter to class

--- Sources/UnitConverter/UnitConverter.swift
+++ Sources/UnitConverter/UnitConverter.swift
@@ -1,6 +1,6 @@
 // Just because the struct is public, doesn't mean everything inside it is too. You must mark everything you want to be externally accessible with an access control level of public.
 
-public struct UnitConverter {
+public class UnitConverter {
     
     private let secretNumber = 42
     
